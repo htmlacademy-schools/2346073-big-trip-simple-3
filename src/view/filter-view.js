72da5869f020afe0ce14bb055447a61e25861af5
IlyaFonichev <il-shturm@mail.ru>
2023-06-07 00:09:10 +0300

    modele4-task2

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,25 +1,34 @@
+import {capitalizeType} from '../utils.js';
 import AbstractView from '../framework/view/abstract-view.js';
 
 export default class FilterView extends AbstractView{
+  #filters = null;
 
-  get template() {
-    return createFilterTemplate();
+  constructor(filters) {
+    super();
+    this.#filters = filters;
   }
 
+  get template() {
+    return createFilterTemplate(this.#filters);
+  }
 }
 
-function createFilterTemplate() {
-  return (
-    `<form class="trip-filters" action="#" method="get">
-    <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
-      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-    </div>
-    <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-      <label class="trip-filters__filter-label" for="filter-future">Future</label>
-    </div>
-    <button class="visually-hidden" type="submit">Accept filter</button>
-  </form>`
+function createFilterItemTemplate(filterType) {
+  return `
+  <div class="trip-filters__filter">
+      <input id="filter-${filterType}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filterType}">
+      <label class="trip-filters__filter-label" for="filter-${filterType}">${capitalizeType(filterType)}</label>
+  </div>
+  `;
+}
+
+function createFilterTemplate(filters) {
+  const filterItems = filters.map((filter) => createFilterItemTemplate(filter)).join('');
+  return (`
+    <form class="trip-filters" action="#" method="get">
+      ${filterItems}
+      <button class="visually-hidden" type="submit">Accept filter</button>
+    </form>`
   );
 }
